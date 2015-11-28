c13e1db7e1fcbe11136495a973016b1b1d1c8454
Patrick Lambert <dendory@live.ca>
2015-11-28 15:54:25 -0500

    code cleanup

--- sample.py
+++ sample.py
@@ -2,7 +2,7 @@ import chartjs
 import csv
 
 # Create a new chart object and set some values
-mychart = chartjs.chart("Wind energy visualization (in million KW/h)", "Line", 1600, 800)
+mychart = chartjs.chart("Wind energy visualization (in million KW/h) for 2010-2012", "Line", 1600, 800)
 
 # Set some basic options
 mychart.set_params(barValueSpacing = 100, scaleShowGridLines = False)
@@ -14,6 +14,7 @@ countries = []
 values2012 = []
 values2011 = []
 values2010 = []
+value = {'2010': 0, '2011': 0, '2012': 0}
 
 # Read data from sample.csv
 f = open("sample.csv", 'r', newline='')
@@ -24,27 +25,23 @@ next(data) # Skip headers
 for line in data:
 	if len(line) > 4:
 		if line[0] not in countries:
+			# Place the values of the last country in our lists
 			if countries != []:
-				values2012.append(value2012)
-				values2011.append(value2011)
-				values2010.append(value2010)
+				values2012.append(value['2012'])
+				values2011.append(value['2011'])
+				values2010.append(value['2010'])
 			# Reset values for the next country
-			value2012 = 0
-			value2011 = 0
-			value2010 = 0
-			# Append this country to the list
+			for k,v in value.items():
+				value[k] = 0
+			# Append this country to the list of labels
 			countries.append(line[0])
 		# Check the three years we care about
-		if line[2] == '2012':
-			value2012 = float(line[4])
-		if line[2] == '2011':
-			value2011 = float(line[4])
-		if line[2] == '2010':
-			value2010 = float(line[4])
+		if line[2] in value.keys():
+			value[line[2]] = float(line[4])
 # Add the last country in the list
-values2012.append(value2012)
-values2011.append(value2011)
-values2010.append(value2010)
+values2012.append(value['2012'])
+values2011.append(value['2011'])
+values2010.append(value['2010'])
 
 # Set labels to be the countries found in our file
 mychart.set_labels(countries)
